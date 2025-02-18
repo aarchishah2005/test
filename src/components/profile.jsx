9dcc157896f9ed70f05f4882f0182739ca82e127
aarchishah2005 <aarchi.shah2005@gmail.com>
2025-02-18 18:28:08 +0530

    Update profile.jsx

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -9,7 +9,7 @@ const BackgroundProfile = () => {
     },
     {
       title: 'Certificates',
-      content: "I'm a paragraph. Click here to add your own text and edit me. It's easy. Just click Edit Text or double click me to add your own content and make changes to the font. I'm a great place for you to tell a story and let your users know a little more about you."
+      content: "This realization sparked a question in my mind – why is finance such a mystery to so many of us, even though we are educated and earning well? I shared my concerns with my father, wondering why some communities seem to have mastered financial literacy from childhood while others struggle. That’s when I decided to face my fear head-on. I immersed myself in learning everything about finance – from investments to managing my taxes – and something incredible happened: it wasn’t as complicated as I had imagined. It was just simple math and, often, common sense!"
     },
     {
       title: 'Specialization',
