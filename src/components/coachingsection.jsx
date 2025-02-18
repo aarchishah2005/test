72637e63864ce7b9504b0a10e08a387e55823823
aarchishah2005 <aarchi.shah2005@gmail.com>
2025-02-18 17:41:51 +0530

    Update coachingsection.jsx

--- src/components/coachingsection.jsx
+++ src/components/coachingsection.jsx
@@ -6,15 +6,25 @@ const CoachingSection = () => {
     <div className="coaching-container">
       <div className="coaching-content">
         <h1 className="coaching-title">
-          Why I Started <br /> Coaching
+         My story of why <br /> Fearless Finance Coaching
         </h1>
 
         <p className="coaching-text">
-          I'm a paragraph. Click here to add your own text and edit me. 
-          It's easy. Just click "Edit Text" or double click me to add 
-          your own content and make changes to the font. I'm a great 
-          place for you to tell a story and let your users know a 
-          little more about you.
+      
+
+Growing up in a business-class family, I thought I had a solid grip on finance. But when I entered the corporate world, something surprising happened – I found myself in the same boat as many others. I, too, handed over my income tax filing to my brother and banking transactions to my husband, believing finance was a realm I didn't fully understand. Even corporate leaders, successful women, admitted that finance was "handled by their husbands."
+
+This realization sparked a question in my mind – why is finance such a mystery to so many of us, even though we are educated and earning well? I shared my concerns with my father, wondering why some communities seem to have mastered financial literacy from childhood while others struggle. 
+
+That’s when I decided to face my fear head-on. I immersed myself in learning everything about finance – from investments to managing my taxes – and something incredible happened: it wasn’t as complicated as I had imagined. It was just simple math and, often, common sense! 
+
+With newfound confidence, I began discussing investments and financial strategies comfortably with my colleagues, realizing that if I could do it, so could others. And that’s when it clicked: why not help others break free from their fear of finance too?
+
+That’s how *Fearless Finance Coaching* was born. It’s time to change the narrative and empower everyone, especially women, to live a financially fearless life. After all, mastering finance isn’t about being a genius – it’s about being fearless and making smart, informed decisions. 
+
+Join me and *Enrich Your Financial Quotient*, master *Fearless Financial Investment*, and dive into *Fearless Financial Trading* – because financial freedom should never be a fear, it should be a way of life.
+
+
         </p>
       </div>
       <button className="learn-more-btn">Learn More</button>
