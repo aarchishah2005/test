51b059e68545de1db84e15092745404958801420
aarchishah2005 <aarchi.shah2005@gmail.com>
2025-02-19 13:28:44 +0530

    Update financialprogram.jsx

--- src/components/financialprogram.jsx
+++ src/components/financialprogram.jsx
@@ -8,9 +8,7 @@ const FinancialPrograms = () => {
         <div className="financial-content">
           <h1 className="financial-title">Financial Programs</h1>
           <p className="financial-text">
-            I'm a paragraph. Click here to add your own text and edit me. It's easy. Just click "Edit Text" or 
-            double click me to add your own content and make changes to the font. I'm a great place for you 
-            to tell a story and let your users know a little more about you.
+            Join me and Enrich Your Financial Quotient  – because financial freedom should never be a fear, it should be a way of life.
           </p>
         </div>
       </div>
