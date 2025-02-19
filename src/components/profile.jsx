eb35448349c751388aa96c65c9a388497ae3c3b0
aarchishah2005 <aarchi.shah2005@gmail.com>
2025-02-19 16:35:08 +0530

    Update profile.jsx

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -13,7 +13,7 @@ const BackgroundProfile = () => {
     },
     {
       title: "Always Through",
-      content: "With newfound confidence, I began discussing investments and financial strategies comfortably with my colleagues, realizing that if I could do it, so could others. And that’s when it clicked: why not help others break free from their fear of finance too? That’s how Fearless Finance Coaching was born. It’s time to change the narrative and empower everyone, especially women, to live a financially fearless life. After all, mastering finance isn’t about being a genius – it’s about being fearless and making smart, informed decisions."
+      content: "With newfound confidence, I began discussing investments and financial strategies comfortably with my friends, families and colleagues, realizing that if I could do it, so could others. And that’s when it clicked: why not help others break free from their fear of finance too? That’s how Fearless Finance Coaching was born. It’s time to change the narrative and empower everyone, especially women, to live a financially fearless life. After all, mastering finance isn’t about being a genius – it’s about being fearless and making smart, informed decisions."
     }
     
       
