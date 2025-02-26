9aaa4fecbc446d413be8a6fa1bd20ccda00b56ae
aarchishah2005 <aarchi.shah2005@gmail.com>
2025-02-26 12:12:20 +0530

    Add files via upload

--- src/components/FinancialCards.jsx
+++ src/components/FinancialCards.jsx
@@ -52,12 +52,6 @@ const FinancialCards = () => {
               />
             </div>
             <h3 className="card-subtitle">{card.subTitle}</h3>
-            <p className="card-text">
-              I'm a paragraph. Click here to add your own text and edit me. It's easy. 
-              Just click "Edit Text" or double click me to add your own content and 
-              make changes to the font. I'm a great place for you to tell a story and let 
-              your users know a little more about you.
-            </p>
             {/* <button className="learn-more-btn">Learn More</button> */}
 
             <Link to={card.buttons[0].link} className="learn-more-btn">
