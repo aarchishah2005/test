9af87cbb54f204ae3f68960975fe378994a1b09f
aarchishah2005 <aarchi.shah2005@gmail.com>
2025-02-26 12:54:48 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -62,7 +62,6 @@ const Header = () => {
         <NavLink to="/about" text="About" onClick={toggleMenu} />
         <NavLink to="https://www.blissquants.com/Bliss_Coaching" text="Coaching" onClick={toggleMenu} />
         <NavLink to="https://www.amazon.in/dp/B09NWH5S7R" text="Book" onClick={toggleMenu} />
-        <NavLink to="/register" text="Register" onClick={toggleMenu} />
         <NavLink to="https://www.blissquants.com/BlissAboutUs#collapseOne" text="Contact" onClick={toggleMenu} />
       </div>
 
