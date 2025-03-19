a8f9baf89bdfd3fb41180dcab439e27acbd3fb8c
Aarchi Shah <aarchi.shah2005@gmail.com>
2025-03-19 16:09:54 +0530

    Update Footer.jsx
    contact url fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -78,7 +78,7 @@ const Footer = () => {
                     <div className="separator">|</div>
                     <a href="https://blissquants.com/BlissAboutUs#collapseThree" className="footer-link" target="_blank" rel="noopener noreferrer">Career</a>
                     <div className="separator">|</div>
-                <a href="https://blissquants.com/BlissAboutUs#collapseFive" className="footer-link" target="_blank" rel="noopener noreferrer">Contact Us</a> 
+                <a href="https://www.falgunivahora.com#contact" className="footer-link" target="_blank" rel="noopener noreferrer">Contact Us</a> 
                 {/* <a href="https://www.falgunivahora.com/#contact" className="footer-link" target="_blank" rel="noopener noreferrer">Contact Us</a>*/}
                 
 {/*                     <span className="footer-link" onClick={scrollToContact} style={{ cursor: "pointer" }}>
