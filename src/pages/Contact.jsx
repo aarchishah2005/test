0f1796f1706b839603174ea944f49fdc3983dec5
Aarchi Shah <aarchi.shah2005@gmail.com>
2025-04-07 21:19:12 +0530

    Add files via upload

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,22 @@
+import Form from "../components/Form"
+import Information from "../components/Information"
+import SocialMedia from "../components/SocialMedia"
+import Update from "../components/Update"
+import Footer from "../components/Footer"
+import "./Contact.css"
+
+function Contact() {
+  
+
+  return (
+    <>
+     <Form/>
+     <Information/>
+     <SocialMedia/>
+     <Update/>
+     <Footer/>
+    </>
+  )
+}
+
+export default Contact
