bcdac7ecf18b453ce9adecb3f03aca7038e87259
Aarchi Shah <aarchi.shah2005@gmail.com>
2025-04-07 21:18:34 +0530

    Add files via upload

---
+++ src/components/Information.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Information.css"
+
+const Information = () => {
+  return (
+    <div class="Information-info">
+<h2>For any other Query you may also Contact us on:</h2>
+<div class="Information-cards">
+    <div class="card">
+        <img src="/assets/email.img.png" alt="Email"/>
+        <p><strong>Email:</strong> Falguni.Vahora@blissquants.com</p>
+    </div>
+    <div class="card">
+        <img src="/assets/msg.img.png" alt="Messenger"/>
+        <p><strong>Messenger:</strong> Chat with us for quick response</p>
+    </div>
+    <div class="card">
+        <img src="/assets/loc.img.png" alt="Address"/>
+        <p><strong>Address:</strong>D - 422,ITC Building, BlissQuants Datalytics LLP, Majuragate, Surat 395002</p>
+    </div>
+</div>
+</div>
+  )
+}
+
+export default Information
---
+++ src/components/Update.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Update.css"
+
+const Update = () => {
+  return (
+    <div className="Update">
+    <h3>GET REGULAR UPDATES FROM MY SIDE, BY SIGNING UP FOR MY NEWSLETTER.</h3>
+    <div className="Update-form">
+        <input type="text" placeholder="First Name"/>
+        <input type="email" placeholder="Email"/>
+        <button>SUBSCRIBE</button>
+    </div>
+</div>
+  )
+}
+
+export default Update
