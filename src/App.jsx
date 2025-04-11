1ab44cbc988650098c02a39349cee8bfa0ef8788
Aarchi Shah <aarchi.shah2005@gmail.com>
2025-04-11 15:28:37 +0530

    Update App.jsx
    import FinanceCoursesPricing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Heading from './components/heading';
 import Button from './components/button';
 import FinancialPrograms from './components/financialprogram';
 import FinancialCards from './components/FinancialCards';
+import FinanceCoursesPricing from './components/FinanceCoursesPricing';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 // import About from "./pages/About";
