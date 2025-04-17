3413b9bd28d801ba51f46819825c700aedb3b199
Aarchi Shah <aarchi.shah2005@gmail.com>
2025-04-17 17:42:06 +0530

    Update Information.jsx
    Information.jsx made some UI changes

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import "./Information.css"
+import React from "react";
+import "./Information.css";
 
 const Information = () => {
   return (
     <div class="Information-info">
-<h2>For any other Query you may also Contact us on:</h2>
-<div class="Information-cards">
-    <div class="card1">
-        <img src="/assets/email.img.png" alt="Email"/>
-        <p><strong>Email:</strong> Inquiry@blissquants.com</p>
+      <h2>For any other Query you may also Contact us on:</h2>
+      <div class="Information-cards">
+        <div class="card1">
+          <img src="/assets/email.img.png" alt="Email" />
+          <p>
+            <strong>Email:</strong> <br />  Inquiry@blissquants.com
+          </p>
+        </div>
+        <div class="card1">
+          <img src="/assets/msg.img.png" alt="Messenger" />
+          <p>
+            <strong>Contact:</strong> <br />  +91 92096 32020
+          </p>
+        </div>
+        <div class="card1">
+          <img src="/assets/loc.img.png" alt="Address" />
+          <p>
+            <strong>Address:</strong> <br /> A11, The Laburnums, Mitcon Road, Balewadi, Pune – 411045, Maharashtra
+          </p>
+        </div>
+      </div>
     </div>
-    <div class="card1">
-        <img src="/assets/msg.img.png" alt="Messenger"/>
-        <p><strong>Messenger:</strong> Chat with us for quick response</p>
-    </div>
-    <div class="card1">
-        <img src="/assets/loc.img.png" alt="Address"/>
-        <p><strong>Address:</strong>A11, The Laburnums, Mitcon Road, Balewadi, Pune – 411045, Maharashtra</p>
-    </div>
-</div>
-</div>
-  )
-}
+  );
+};
 
-export default Information
+export default Information;
