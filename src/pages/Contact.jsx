fb034f46b25973af6c68b6bd0346acdb15d54aa9
Aarchi Shah <aarchi.shah2005@gmail.com>
2025-04-19 17:30:50 +0530

    Update Contact.jsx
    Add upadated contact us form to it

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 // import Form from "../components/Form"
-import ContactUsForm from "../components/ContactUsForm"
+import ContactForm from "../components/ContactForm"
 import Information from "../components/Information"
 // import SocialMedia from "../components/SocialMedia"
 // import Update from "../components/Update"
@@ -16,7 +16,27 @@ function Contact() {
     <div className="contact">
     <>
      {/* <Form/> */}
-     <ContactUsForm/>
+    <ContactForm
+          infoText={
+            <>
+              <span style={{ fontWeight: "bold" }}>Let's Connect</span>
+              <br />
+              <br />
+              Falguni Vahora is a fearless finance leader, investor, and
+              advocate for financial empowerment, especially for women.
+              <br />
+              <br />
+              A BE-EC graduate from NIT Surat, she has 27+ years of experience
+              spanning software development, teaching, finance, and stock market
+              trading. She co-founded BlissQuants Analytics, leads a trading
+              desk of 40+ professionals, and has trained 800+ individuals in
+              investment and trading. A SEBI-registered research analyst and
+              author of Mastering Options Delta Trading, she blends expertise in
+              data analytics, risk management, and coaching to transform
+              financial education.
+            </>
+          }
+        />
      <Information/>
      {/* <SocialMedia/>
      <Update/> */}
