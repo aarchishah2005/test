f3f2d422b6b68e18b52f03f919957640393cf03e
Aarchi Shah <aarchi.shah2005@gmail.com>
2025-04-21 23:22:59 +0530

    Update InvestmentEducation.jsx
    text chnged by fal

--- src/components/InvestmentEducation.jsx
+++ src/components/InvestmentEducation.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './InvestmentEducation.css';
-
-const InvestmentEducation = () => {
-  const features = [
-    "Learn to take control of your finance in your hand",
+{/*  "Learn to take control of your finance in your hand",
     "Build the confidence & mindset needed to invest successfully",
     "Jumpstart your stock market journey and work part time",
     "Invest using simple strategy based on CAGR, Risk and Return",
-    "No prior experience required to attend"
+    "No prior experience required to attend"  */} 
+
+const InvestmentEducation = () => {
+  const features = [
+   
+   "Take Control of Your Finances — No Experience Needed!",
+   "Learn to manage your money with clarity and confidence",
+   "Build the mindset needed to invest fearlessly",
+   "Start your stock market journey — even part-time",
+   "Use simple strategies based on CAGR, risk & return",
+   "No jargon. No confusion. Just practical tools that work"
   ];
 
   return (
