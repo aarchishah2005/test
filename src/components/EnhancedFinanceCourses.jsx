473e8606f422b395891b57077f4eefe22b1ee79f
Aarchi Shah <aarchi.shah2005@gmail.com>
2025-04-21 13:07:18 +0530

    Update EnhancedFinanceCourses.jsx

--- src/components/EnhancedFinanceCourses.jsx
+++ src/components/EnhancedFinanceCourses.jsx
@@ -6,7 +6,7 @@ const EnhancedFinanceCourses = () => {
   const challenges = [
     {
       id: 1,
-      title: "Fearless Financial Investment – 1-Day Workshop",
+      title: "Fearless Financial Investment :: 1-Day Workshop",
       description:
         "Break free from financial fear and take charge of your investments with confidence! This power-packed workshop will equip you with essential finance concepts, expert stock market strategies, AI-driven stock selection, and risk management techniques. With hands-on tools, personalized guidance, and a strong investment community, you’ll gain the skills to invest smartly and fearlessly. Join us and start building your financial freedom today!",
       startDate: "24 May 2025",
@@ -21,7 +21,7 @@ const EnhancedFinanceCourses = () => {
     },
     {
       id: 2,
-      title: "Fearless Financial Trading 3-day online live course!",
+      title: "Fearless Financial Trading :: 3-day online live course",
       description:
         "The Fearless Financial Trading 3-day flagship course is designed to transform you into a confident and strategic trader. Whether you're a beginner or looking to refine your trading skills, this intensive program equips you with cutting-edge strategies, AI-driven insights, and proven trading systems to help you trade fearlessly. With hands-on training, expert guidance, and a structured approach, you'll gain the confidence to navigate the stock market like a pro. Join us and unlock your path to financial freedom today!",
       startDate: "16 May 2025",
@@ -33,7 +33,7 @@ const EnhancedFinanceCourses = () => {
     },
     {
       id: 3,
-      title: "Fearless Options Mastery – 3-Month online live Course",
+      title: "Fearless Options Mastery :: 3-Month online live Course",
       description:
         "Unlock the secrets of successful options trading with our Fearless Options Mastery course! In just 3 months, gain deep expertise in derivatives, Greeks, delta-neutral strategies, and IV-based trading systems with real-time practice and expert mentorship. Get equipped with powerful tools, live trading setups, risk management systems, and weekly community support. Plus, prepare for the NISM Equity Derivatives Certification and fast-track your trading journey with readymade strategies. Take control of your financial future and trade fearlessly!",
       startDate: "2 June 2025",
