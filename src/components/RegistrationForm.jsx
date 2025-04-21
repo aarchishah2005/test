0618c9821774191377f4fa6e79836631535642e1
Aarchi Shah <aarchi.shah2005@gmail.com>
2025-04-21 22:14:05 +0530

    Update RegistrationForm.jsx
    remove some text

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -25,17 +25,17 @@ const RegistrationForm = () => {
           <ul className="secrets-list">
             <li>
               <span className="checkmark">✓</span>
-              <span><span className="secret-number">Secret 1:</span> The Simple Way to Build a Strong Financial Base — Even If You’ve Always Avoided Numbers </span>
+              <span><span className="secret-number">Secret 1:</span> The Simple Way to Build a Strong Financial Base</span>
              
              {/* Strengthen Your Financial Foundation    Unlock Ready-Made Investment Strategy           Start Your Part-Time Journey in the Stock Market*/}
             </li>
             <li>
               <span className="checkmark">✓</span>
-              <span><span className="secret-number">Secret 2:</span>Use a Proven Investment Strategy That Works — No Experience Needed </span>
+              <span><span className="secret-number">Secret 2:</span>Use a Proven Investment Strategy That Works</span>
             </li>
             <li>
               <span className="checkmark">✓</span>
-              <span><span className="secret-number">Secret 3:</span>How to Start Earning from the Stock Market Part-Time — While Keeping Your Job or Business </span>
+              <span><span className="secret-number">Secret 3:</span>How to Start Earning from the Stock Market Part-Time</span>
             </li>
           </ul>
         </div>
