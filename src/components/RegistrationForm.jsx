4cb1f33b14261150fefc2d6c9f3059254b15c2ee
Aarchi Shah <aarchi.shah2005@gmail.com>
2025-04-21 21:41:42 +0530

    Update RegistrationForm.jsx
    chnged secrets text by fal

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -25,15 +25,17 @@ const RegistrationForm = () => {
           <ul className="secrets-list">
             <li>
               <span className="checkmark">✓</span>
-              <span><span className="secret-number">Secret 1:</span> Strengthen Your Financial Foundation</span>
+              <span><span className="secret-number">Secret 1:</span> The Simple Way to Build a Strong Financial Base — Even If You’ve Always Avoided Numbers </span>
+             
+             {/* Strengthen Your Financial Foundation    Unlock Ready-Made Investment Strategy           Start Your Part-Time Journey in the Stock Market*/}
             </li>
             <li>
               <span className="checkmark">✓</span>
-              <span><span className="secret-number">Secret 2:</span> Unlock Ready-Made Investment Strategy</span>
+              <span><span className="secret-number">Secret 2:</span>Use a Proven Investment Strategy That Works — No Experience Needed </span>
             </li>
             <li>
               <span className="checkmark">✓</span>
-              <span><span className="secret-number">Secret 3:</span> Start Your Part-Time Journey in the Stock Market</span>
+              <span><span className="secret-number">Secret 3:</span>How to Start Earning from the Stock Market Part-Time — While Keeping Your Job or Business </span>
             </li>
           </ul>
         </div>
