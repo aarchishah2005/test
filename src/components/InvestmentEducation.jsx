11f51c5e01f31f7af827a2cfb636486f3e82fb91
shahforamb <shahforamb@gmail.com>
2025-04-23 12:45:27 +0530

    Update InvestmentEducation.jsx
    updated text

--- src/components/InvestmentEducation.jsx
+++ src/components/InvestmentEducation.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import './InvestmentEducation.css';
-{/*  "Learn to take control of your finance in your hand",
+import React from "react";
+import "./InvestmentEducation.css";
+{
+  /*  "Learn to take control of your finance in your hand",
     "Build the confidence & mindset needed to invest successfully",
     "Jumpstart your stock market journey and work part time",
     "Invest using simple strategy based on CAGR, Risk and Return",
-    "No prior experience required to attend"  */} 
+    "No prior experience required to attend"  */
+}
 
 const InvestmentEducation = () => {
   const features = [
-   
-   "Take Control of Your Finances — No Experience Needed!",
-   "Learn to manage your money with clarity and confidence",
-   "Build the mindset needed to invest fearlessly",
-   "Start your stock market journey — even part-time",
-   "Use simple strategies based on risk & return",
-   "No jargon. No confusion. Just practical tools that work"
+    "Take Control of Your Finances — No Experience Needed!",
+    "Learn to manage your money with clarity and confidence",
+    "Build the mindset needed to invest fearlessly",
+    "Start your stock market journey — even part-time",
+    "Use simple strategies based on risk & return",
+    "No jargon. No confusion. Just practical tools that work",
   ];
 
   return (
