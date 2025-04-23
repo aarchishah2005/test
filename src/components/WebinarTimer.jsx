aca575d8c97c977f79b6dd53fea89fe1911db6fa
shahforamb <shahforamb@gmail.com>
2025-04-23 12:52:51 +0530

    Create WebinarTimer.jsx

---
+++ src/components/WebinarTimer.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import "./WebinarTimer.css"; // Ensure this file handles responsiveness
+import DateDay from "./DateDay";
+import { useWebinarTimer } from "./useWebinarTimer";
+
+const WebinarTimer = () => {
+  const timeLeft = useWebinarTimer("2025-04-26T23:59:59");
+  return (
+    <div className="webinar-container">
+      <DateDay
+        date="26 April 2025"
+        day="Saturday"
+        startTime="2 : 00 PM"
+        endTime="4 : 00 PM"
+      />
+
+      <div className="countdown">
+        <div className="time-block">
+          <div className="time">
+            <span>{timeLeft.days}d</span> :
+          </div>
+          <div className="label">DAYS</div>
+        </div>
+        <div className="time-block">
+          <div className="time">
+            <span>{timeLeft.hours}h</span> :
+          </div>
+          <div className="label">HOURS</div>
+        </div>
+        <div className="time-block">
+          <div className="time">
+            <span>{timeLeft.minutes}m</span> :
+          </div>
+          <div className="label">MINUTES</div>
+        </div>
+        <div className="time-block">
+          <div className="time">
+            <span>{timeLeft.seconds}s</span>
+          </div>
+          <div className="label">SECONDS</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WebinarTimer;
