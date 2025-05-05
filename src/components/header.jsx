f9a28f8e4819ceb77c406033a1a78e8125d0c930
Aarchi Shah <aarchi.shah2005@gmail.com>
2025-05-05 13:57:28 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -69,8 +69,7 @@ const Header = () => {
         <NavLink to="/" text="Home" onClick={toggleMenu} />
 {/*         <NavLink to="/about" text="About" onClick={toggleMenu} /> */}
      {/*   <NavLink to="/Coaching" text="Coaching" onClick={toggleMenu} /> */}
-{/*          <NavLink to="/EfqLanding" text="Session" onClick={toggleMenu} /> {/* Updated Contact */} 
-        <NavLink to="/EfqLanding_kids" text="Session" onClick={toggleMenu} /> {/* Updated Contact */}
+         <NavLink to="/EfqLanding" text="Session" onClick={toggleMenu} />
         <NavLink to="https://www.amazon.in/dp/B09NWH5S7R" text="Book" onClick={toggleMenu} />
         <NavLink to="/Contact" text="Contact" onClick={scrollToContact} />
         
