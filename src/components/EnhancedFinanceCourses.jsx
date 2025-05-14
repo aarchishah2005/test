42505a3a53b2758b98f1520932688fa0a65e720d
Aarchi Shah <aarchi.shah2005@gmail.com>
2025-05-14 14:41:50 +0530

    Update EnhancedFinanceCourses.jsx

--- src/components/EnhancedFinanceCourses.jsx
+++ src/components/EnhancedFinanceCourses.jsx
@@ -23,9 +23,19 @@ const EnhancedFinanceCourses = () => {
     {
       id: 2,
       title: "Fearless Financial Trading :: 3-day online live course",
-      description:
-          "Learn  trading with confidence in just 3 days! This live, intensive course is perfect for beginners and aspiring traders ready to trade fearlessly.Course Highlights:•	Trading Basics: Charts, OHLC, candlestick patterns, Technical Analysis •	Key Indicators: MA, EMA, RSI, VWAP, Support & Resistance•	Trading Psychology: Mindset, risk management •	Systems & Strategy: Entry/Exit, Targets, Stop Loss/Trailing Stop Loss•	Money Management: Position sizing, risk/reward, trade journaling•	Conviction Building: News, global data, price action, backtesting•	Lifetime Support: Weekly live face-to-face doubt-solving sessions ",
-            
+      description: (
+        <>
+        Learn  trading with confidence in just 3 days! This live, intensive course is perfect for beginners and aspiring traders ready to trade fearlessly.<br />
+        Course Highlights:<br />
+        	•	<strong>Trading Basics:</strong> Charts, OHLC, candlestick patterns, <strong>Technical Analysis</strong> <br />
+        	•	<strong>Key Indicators:</strong> MA, EMA, RSI, VWAP, Support & Resistance<br />
+        	•	<strong>Trading Psychology:</strong> Mindset, <strong>risk management </strong><br />
+        	•	<strong>Systems & Strategy:</strong> Entry/Exit, Targets, Stop Loss/Trailing Stop Loss<br />
+        	•	<strong>Money Management:</strong> Position sizing, risk/reward, trade journaling<br />
+        	•	<strong>Conviction Building:</strong> News, global data, price action, backtesting<br />
+        	•	<strong>Lifetime Support:</strong> Weekly live <strong>face-to-face doubt-solving sessions</strong><br />
+        </>
+      ),
       startDate: "20, 21, 22 June 2025",
       duration: "3 Days",
       language: "Hindi & English",
